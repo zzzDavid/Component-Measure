e118d8146de01ce85f4dc326a5ed700141e664b7
Patrick <yangsy9@mail2.sysu.edu.cn>
2018-10-22 16:15:17 +0800

    Remove dummy tests for the real stuff

--- Measurement.UnitTests/Measurement.UnitTests/Tests.cs
+++ Measurement.UnitTests/Measurement.UnitTests/Tests.cs
@@ -7,26 +7,80 @@ namespace Measurement.UnitTests
     public class MeasurementTests
     {
         [Test]
-        public void DummyContourLines_Test1()
+        public void Measurement_BasicLoading()
         {
             // Arrange - 1st phase of a Unit Test. Get the thing you need
-            var x = new Measurement();
+            const string filename = "path/to/your/image.tif"; // change this to a valid path
+            var m = new Measurement(filename);
 
-            const int testImage = 190087;
-            string[] testLines =
-            {
-                "   Hello  ",
-                "   World",
-                "Seeya          "
-            };
-            
             // Act - 2nd. Do some stuff
-            x.DummyContourLines(testImage, testLines);
-            
-            // Assert - 3rd. Check if the stuff are done write (or print them)
-            x.DummyPrintContourLines();
-            Assert.True(true);
-            Console.WriteLine("DummyContourLines_Test1 exiting (salute).");
+            const string anotherFilename = "path/to/another/image.tif";
+            const double scaling = 0.5;
+            m.Load(anotherFilename, scaling);
+
+            // Should properly display the image
+            m.Display();
+
+            // Access the image
+            // Requires EMGU.CV in this Solution. Should we add another 200MB?
+            // Emgu.CV.Structure.Mat loadedImage = m.Image;
+
+            Console.WriteLine("Measurement_BasicLoading exiting (salute).");
+        }
+
+        [Test]
+        public void FourCorners_Return8CoordinatesDoubleArray() // Function 1 in the PDF
+        {
+            // Arrange
+            const string filename = "path/to/image";
+            var m = new Measurement(filename);
+
+            // Act
+            // Should return the coordinates of four corners, clockwise
+            /*
+             * (AX, AY)-------------------------(BX, BY)
+             * |                                       |
+             * |                                       |
+             * |                                       |
+             * (DX, DY)-------------------------(CX, CY)
+             * 
+             */
+            var corners = new double[8];
+            var status = "";
+            m.FourCorners(ref corners, ref status);
+
+            // Assert
+            Assert.That(corners[0] /* AX */ < corners[2] /* BX */);
+            Assert.That(corners[0] /* AX */ < corners[6] /* DX */);
+            Assert.That(corners[2] /* BX */ < corners[4] /* CX */);
+            Assert.That(corners[6] /* DX */ < corners[4] /* CX */);
+            Assert.That(corners[1] /* AY */ < corners[3] /* BY */);
+            Assert.That(corners[1] /* AY */ < corners[7] /* DY */);
+            Assert.That(corners[3] /* BY */ < corners[5] /* CY */);
+            Assert.That(corners[7] /* DY */ < corners[5] /* CY */);
+            Console.WriteLine(status);
+        }
+
+        [Test]
+        public void RectangleData_IsRectangle_ReturnHeightWidthDiagonal_ReturnTrueIfRectangle() // Function 2 in PDF
+        {
+            const string rectFilename = "path/to/rectangle/part";
+            const string notRectFilename = "path/to/not-rectangle/part";
+            var m1 = new Measurement(rectFilename);
+            var m2 = new Measurement(notRectFilename);
+
+            Assert.IsTrue(m1.IsRectangle());
+            Assert.IsFalse(m2.IsRectangle());
+
+            var data = new double[3];
+            var status = "";
+            m1.RectangleData(ref data, ref status);
+            Console.WriteLine(string.Format("height = {0}, width = {1}, diagonal = {2}", data[0], data[1], data[2]));
+            Console.WriteLine(status);
+
+            m2.RectangleData(ref data, ref status);
+            // Proper warning should be given when it is not a rectangle
+            Console.WriteLine(status);
         }
     }
 }
